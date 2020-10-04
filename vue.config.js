35ce79cd7e85178c4100b075d66b634ff71cadb0
wiloon <wiloon.wy@gmail.com>
2020-10-04 13:48:44 +0800

    gin

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
         ws: true,
         changeOrigin: true,
         pathRewrite: {

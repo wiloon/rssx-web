51821cf40dae1782b30b65b2206dd544ae1b5490
wiloon <wiloon.wy@gmail.com>
2020-10-11 14:08:02 +0800

    pwa test

---
+++ src/service-worker.js
@@ -0,0 +1,45 @@
+workbox.core.setCacheNameDetails({
+  prefix: 'rssx',
+  suffix: 'v1'
+})
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/*
+* vue-cli3.0通过workbox-webpack-plagin 来实现相关功能，我们需要加入
+* 以下语句来获取预缓存列表和预缓存他们，也就是打包项目后生产的html，js，css等* 静态文件
+*/
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  /\/api/, new workbox.strategies.NetworkOnly()
+)
+workbox.routing.registerRoute(
+  /\.(jpe?g|png)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-runtime-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // 对图片资源缓存 1 星期
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        // 匹配该策略的图片最多缓存 10 张
+        maxEntries: 10
+      })
+    ]
+  })
+)
+workbox.routing.registerRoute(
+  /\.(css|js)/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'css_js-runtime-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // 对图片资源缓存 1 星期
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        // 匹配该策略的图片最多缓存 10 张
+        maxEntries: 10
+      })
+    ]
+  })
+)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  pwa: {
+    // 一些基础配置
+    name: 'RSSX',
+    assetsVersion: '1.0.0',
+    themeColor: '#4DBA00',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'debault',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // 自定义的service worker文件的位置
+      swSrc: 'src/service-worker.js'
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
